8c58d5293d6ae104de4df7f9bb216c6d491b4338
izg0820 <izg0820@naver.com>
2022-05-10 23:35:47 +0900

    Initial commit

---
+++ src/test/java/sample.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class sample {
+
+    private static List<String> list;
+
+    @BeforeAll
+    public static void setUp() {
+        list = Arrays.asList("A", "B", "C");
+    }
+
+    @Test
+    public void isEqualTo() {
+        assertThat(list.size()).isNotZero().isEqualTo(3);
+    }
+
+    /*
+    * contains : 순서 상관없이 기대값이 포함되어 있으면 pass
+    * containsAll : 순서 상관없이 기대값이 모두 포함되어 있으면 pass (컬렉션)
+    * containsExactly : 순서와 요소가 정확히 일치해야 pass
+    * containsSequence : 중간요소를 건너뛰지 않는 순서와 요소가 일치해야 pass
+    * containsSubsequence : 중간요소를 건너뛰어도 되자만 순서를 바꿔서는 안됨
+    * */
+    @Test
+    public void contains() {
+        //fail
+//        assertThat(list).contains("D")
+//                .containsAll(Arrays.asList("A", "D"))
+//                .containsExactly("A", "C", "B")
+//                .containsSequence("A", "C")
+//                .containsSubsequence("B", "A");
+
+        //pass
+        assertThat(list).contains("A")
+                .containsAll(Arrays.asList("A", "C", "B"))
+                .containsAll(Arrays.asList("A", "B"))
+                .containsExactly("A", "B", "C")
+                .containsSequence("A", "B")
+                .containsSequence("B", "C")
+                .containsSubsequence("B", "C")
+                .containsSubsequence("A", "C");
+    }
+}
