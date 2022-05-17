347777e2efd37ae12650f0ba473c2e029a9f16e6
izg0820 <izg0820@naver.com>
2022-05-17 22:38:46 +0900

    as에 대한 학습(수정)

--- src/test/java/asSample.java
+++ src/test/java/asSample.java
@@ -9,10 +9,12 @@ public class asSample {
 
     /*
     * as는 assert의 설명을 입력할 때 사용된다.
+    * assertion 뒤에 사용하면 재대로 동작하지 않는다..
     * */
     @Test
     public void as() {
         String sample = "1|2|3";
+        assertThat(sample.split("\\|")).containsExactly("4").as("여기에다가 쓰면 동작 X");
         assertThat(sample.split("\\|")).as("테스트1").containsExactly("1","2","3");
         assertThat(sample.split("\\|")).as("테스트2").contains("1");
     }
